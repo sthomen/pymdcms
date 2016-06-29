52ec27f8f55f32f3db92cdf9cc2c0040f01d39ea
Staffan Thomén <duck@shangtai.net>
2016-06-30 02:04:43 +0300

    Format sensors to known values and change 'mib' to 'oid' where appropriate.

--- apps/sensors.py
+++ apps/sensors.py
@@ -5,6 +5,7 @@ from threading import Thread
 from time import sleep
 from datetime import datetime
 from easysnmp import Session,EasySNMPNoSuchObjectError
+import json
 
 from applet import Applet
 
@@ -30,12 +31,12 @@ class Sensors(Applet):
 		# settle with this approach.
 
 		if not Sensors.sensors:
-			if config.has_option('sensors', 'hostname') and config.has_option('sensors', 'community') and config.has_option('sensors', 'version') and config.has_option('sensors', 'mib'):
+			if config.has_option('sensors', 'hostname') and config.has_option('sensors', 'community') and config.has_option('sensors', 'version') and config.has_option('sensors', 'oid'):
 
 				Sensors.sensors=SnmpSensors(config.get('sensors', 'hostname'),
 					config.get('sensors', 'community'),
 					config.get('sensors', 'version'),
-					config.get('sensors', 'mib'))
+					config.get('sensors', 'oid'))
 
 				Sensors.sensors.start()
 
@@ -46,16 +47,23 @@ class Sensors(Applet):
 			return self.show_sensors()
 
 	def show_sensors(self):
+		# because the ajax code below manipulates the object metadata
+		if 'content-type' in self.metadata:
+			del self.metadata['content-type']
+
+		if 'template' in self.metadata:
+			del self.metadata['template']
 
 		return self.render('display', {'data': self.sensors.data, 'updated': self.sensors.updated})
 
 	def ajax_sensors(self):
+		self.metadata['content-type']='application/json'
 		self.metadata['template']='ajax'
 
-		return self.sensors.data
+		return json.dumps(self.sensors.data)
 		
 class SnmpSensors(Thread):
-	def __init__(self, hostname, community, version, mib, polltime=60):
+	def __init__(self, hostname, community, version, oid, polltime=60):
 		Thread.__init__(self, name="SnmpSensors")
 
 		self.daemon=True
@@ -63,7 +71,7 @@ class SnmpSensors(Thread):
 		self.hostname=hostname
 		self.community=community
 		self.version=version
-		self.mib=mib
+		self.oid=oid
 		self.polltime=polltime
 
 		self.updated=None
@@ -86,7 +94,35 @@ class SnmpSensors(Thread):
 				break
 
 	def query_sensors(self):
-		if self.mib:
+		if self.oid:
 			session=Session(hostname=self.hostname, community=self.community, version=int(self.version))
-			self.data=session.walk(self.mib)
+			self.data=self.flip_snmp_data(session.walk(self.oid))
 			self.updated=datetime.now()
+
+	def flip_snmp_data(self, data):
+		"""
+		This method takes the data returned from the walk method (easysnmp.SNMPValue) and
+		changes it from a bare list to a nested directory:
+		{
+			index: {
+				module: value
+				...
+			}
+		}
+		"""
+
+		result={}
+
+		for sv in data:
+			oid=sv.oid.split('.')
+			value=sv.value
+
+			key,index=oid[-2:]
+
+			if not index in result.keys():
+				result[index]={}
+
+			result[index]['oid']='.'.join(oid)
+			result[index][int(key)]=value
+
+		return result
