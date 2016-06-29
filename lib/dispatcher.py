5e0e27bded000c4231ae9c2ddbe4863b1c6378e9
Staffan Thomén <duck@shangtai.net>
2016-06-30 02:01:08 +0300

    Add support for changing the content-type of pages, and add an AJAX template

--- lib/dispatcher.py
+++ lib/dispatcher.py
@@ -28,7 +28,12 @@ class Dispatcher(object):
 	def default(self, *args, **kwargs):
 		if len(args) > 0:
 			if args[0] in self.pages.keys():
-				return self.pages[args[0]].render(args, kwargs)
+				output=self.pages[args[0]].render(args, kwargs)
+
+				if 'content-type' in self.pages[args[0]].metadata:
+					cherrypy.response.headers['Content-Type']=self.pages[args[0]].metadata['content-type']
+
+				return output
 			else:
 				raise cherrypy.HTTPError(404)
 
