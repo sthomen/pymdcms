06bbf00f6bd43677b0d5b36cee9edadfa1d27ef7
Staffan Thomén <duck@shangtai.net>
2016-06-29 23:47:39 +0300

    Add Applet superclass

---
+++ apps/applet.py
@@ -0,0 +1,21 @@
+# vim:ts=4:sw=4:
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+class Applet(object):
+	def __init__(self, config, menus):
+		self.lookup=TemplateLookup(
+			directories=[config.get('global', 'theme')],
+			default_filters=['decode.utf8'],
+			input_encoding='utf-8',
+			output_encoding='utf-8')
+		
+		self.config=config
+		self.menus=menus
+
+	def add_template_dir(self, path):
+		self.lookup.directories.append(path)
+
+	def render(self, template, data={}):
+		return self.lookup.get_template(template).render(**data)
